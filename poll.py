19e133c29b15920d61d3bb182723b4f2e4a04812
Morgan Collett <morgan.collett@gmail.com>
2007-08-13 12:15:54 +0100

    Some PEP-8 love for Carlos' lesson plan widget

--- poll.py
+++ poll.py
@@ -1563,20 +1563,33 @@ def justify(textdict, choice):
 
 class LessonPlanWidget (gtk.Notebook):
     def __init__ (self, basepath):
+        """Create a Notebook widget for displaying lesson plans in tabs.
+
+        basepath -- string, path of directory containing lesson plans.
+        """
         super(LessonPlanWidget, self).__init__()
-        lessons = filter(lambda x: os.path.isdir(os.path.join(basepath, 'lessons', x)), os.listdir(os.path.join(basepath, 'lessons')))
+        lessons = filter(lambda x: os.path.isdir(os.path.join(basepath,
+                                                              'lessons', x)),
+                         os.listdir(os.path.join(basepath, 'lessons')))
         lessons.sort()
         for lesson in lessons:
-            self._load_lesson(os.path.join(basepath, 'lessons', lesson), _(lesson))
+            self._load_lesson(os.path.join(basepath, 'lessons', lesson),
+                              _(lesson))
 
     def _load_lesson (self, path, name):
+        """Load the lesson content from a .abw, taking l10n into account.
+
+        path -- string, path of lesson plan file, e.g. lessons/Introduction
+        lesson -- string, name of lesson
+        """
         code, encoding = locale.getdefaultlocale()
         canvas = AbiCanvas()
         canvas.show()
         files = map(lambda x: os.path.join(path, '%s.abw' % x),
-                    ('_'+code.lower(), '_'+code.split('_')[0].lower(), 'default'))
+                    ('_'+code.lower(), '_'+code.split('_')[0].lower(), 
+                     'default'))
         files = filter(lambda x: os.path.exists(x), files)
-        # On jhbuild, the first works, on XO the second works:
+        # On jhbuild, the first works, on XO image 432 the second works:
         try:
             canvas.load_file('file://%s' % files[0], 'text/plain')
         except:
